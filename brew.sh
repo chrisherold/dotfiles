1debc75a8f673674053a133745c1441019e8bcb8
Chris Herold <chrisherold@users.noreply.github.com>
2017-02-27 14:30:43 -0800

    update brew.sh
    update composer link again

--- brew.sh
+++ brew.sh
@@ -37,7 +37,7 @@ brew install wget --with-iri
 # Install our custom packages:
 
 brew install awscli
-brew install php/composer
+brew install homebrew/php/composer
 brew install git
 # brew install heroku-toolbelt
 brew install hub
