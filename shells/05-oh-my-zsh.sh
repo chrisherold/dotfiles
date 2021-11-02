330beb2e5e3b2ddac6295f803ac24836a35b89d6
Chris Herold <cmherold@gmail.com>
2021-11-02 14:27:05 -0700

    remove iterm completions

--- shells/05-oh-my-zsh.sh
+++ shells/05-oh-my-zsh.sh
@@ -7,7 +7,6 @@ print_info "Installing..."
 
 if [[ ! -d "$HOME/.oh-my-zsh" ]]; then
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-  test -e ${HOME}/.iterm2_shell_integration.zsh && source ${HOME}/.iterm2_shell_integration.zsh
   
   #add poetry to oh my zsh
   mkdir $ZSH_CUSTOM/plugins/poetry
