c4d3f64edb7e1ea195f77432480d04c85fbf55b7
Chris Herold <cmherold@gmail.com>
2021-11-02 14:27:55 -0700

    cleanup

--- shells/05-oh-my-zsh.sh
+++ shells/05-oh-my-zsh.sh
@@ -7,7 +7,7 @@ print_info "Installing..."
 
 if [[ ! -d "$HOME/.oh-my-zsh" ]]; then
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-  
+
   #add poetry to oh my zsh
   mkdir $ZSH_CUSTOM/plugins/poetry
   poetry completions zsh > $ZSH_CUSTOM/plugins/poetry/_poetry 
