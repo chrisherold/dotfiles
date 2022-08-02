81af6c025c150ceb4a18a79bcba364e378e030d3
Chris Herold <cmherold@gmail.com>
2022-08-02 16:43:47 -0700

    simple oh my zsh

--- shells/05-oh-my-zsh.sh
+++ shells/05-oh-my-zsh.sh
@@ -6,10 +6,6 @@ if [[ ! -d "$HOME/.oh-my-zsh" ]]; then
   print_info "Installing..."
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-  #add poetry to oh my zsh
-  mkdir $ZSH_CUSTOM/plugins/poetry
-  poetry completions zsh > $ZSH_CUSTOM/plugins/poetry/_poetry 
-
   print_success "Completed..."
 else
   print_success "Skipping..."
